92b0f120c0bc44c2d8326ef269bcb8f3faadcee7
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-07-15 16:32:41 +0530

    responsive design for smaller screens

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import Sidenav from './navigation/Sidenav';
 import Timeline from './timeline/Timeline';
 import "./HomePage.css";
@@ -16,3 +16,24 @@ const HomePage = () => {
 }
 
 export default HomePage
+*/
+
+import React from 'react';
+import Sidenav from './navigation/Sidenav';
+import Timeline from './timeline/Timeline';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="homepage_nav">
+        <Sidenav />
+      </div>
+      <div className="homepage_timeline">
+        <Timeline />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
